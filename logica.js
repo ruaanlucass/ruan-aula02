2210f6c36b6b9e53ceca5eeaab5fcb3b985c299c
ruaanlucass <lruan9440@gmail.com>
2021-11-09 19:04:11 -0300

    Processo de input e output

---
+++ logica.js
@@ -0,0 +1,12 @@
+const input = require("readline-sync")
+
+const a = "abacaxi"; 
+const b = "banana";
+const c = "laranja";
+const d = "maçã";
+const e = "morango";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
