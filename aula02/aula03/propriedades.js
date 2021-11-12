062a5482c04ecccc18bc6832dc6202ca68d46c4a
ruaanlucass <lruan9440@gmail.com>
2021-11-11 21:15:19 -0300

    Atualização com Propriedades

---
+++ aula02/aula03/propriedades.js
@@ -0,0 +1,46 @@
+/*let frase = "Ruan é lindo"
+console.log(frase.length)*/
+
+//------------------------------
+
+/*var senha = "123458"
+
+if (senha.length == 8) {
+    console.log("a senha está certa")
+} else if (senha.length <= 7) {
+    console.log("Pense direitin")
+}  else {
+    console.log("A senha está incorreta")
+}*/
+
+//-------------------------------------------
+
+/*let salario = 1000
+while (salario < 5000) {
+    salario += 100;
+console.log("O Salário ainda é R$" + salario); }*/
+
+/*let contador = 0
+while (contador < 50) {
+    console.log("repetição nº " + contador);
+    contador++;
+}*/
+
+/*const linguagens = ["JavaScript" , "Python" , "c#" , "PHP"];
+let index = 0
+do {
+    console.log(linguagens[index]);
+
+    index++
+} while (index < linguagens.length);*/
+
+//------------------------------------------------------------------
+
+/*for (var i = 0; i < 9; i++) {
+    console.log(i);
+}*/
+
+for (var i = 0;; i++) {
+    console.log(i);
+    if (i > 3) break;
+}
